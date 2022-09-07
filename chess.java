ae6559abd3d4a0702248054b9dd1bf837ed45486
vidhya-a-mariappan <112065733+vidhya-a-mariappan@users.noreply.github.com>
2022-09-07 16:52:00 +0530

    Create chess.java

---
+++ chess.java
@@ -0,0 +1,7 @@
+public class chess {
+    public static void main(String[] args){
+        String res;
+        res=String.valueOf((Math.pow(2,65))-1);//formula for 2 power series 2^(n+1)-1
+        System.out.println(String.valueOf(res));
+    }
+}
